a1ff6357928ecdabc0d0244320a6937507265b48
Ondrej Špánik <spanik11@gmail.com>
2025-10-23 23:56:59 +0200

    fixed angular

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -3,6 +3,7 @@ import axios, { AxiosResponse } from 'axios';
 
 @Component({
   selector: 'app-root',
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
